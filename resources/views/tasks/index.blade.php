9e420716a5c7dd08482eea50d42313e66bfab19c
shigeoimafuku <pukurinshige@gmail.com>
2022-05-17 05:04:20 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>タスクたち</h1>
+
+    @if (count($tasks)>0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク内容</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show',$task->id,['task'=>$task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    {!! link_to_route('tasks.create','新規タスクの入力',[],['class'=>'btn btn-primary']) !!}
+    
+@endsection
