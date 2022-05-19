123ee42cc3795180ba56e358ad8965f80bdfc772
shigeoimafuku <pukurinshige@gmail.com>
2022-05-19 12:44:01 +0000

    add colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,7 +40,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',]);
         $task=new Task;
+        $task->status=$request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -82,7 +86,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',
+            ]);
         $task=Task::findOrFail($id);
+        $task->status=$request->status;
         $task->content=$request->content;
         $task->save();
         
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,23 +8,11 @@
     </head>
     
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">TaskList</a>
-                
-                <button type="button" class="navbar-toggler" data-toggle="collaspe" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collaspe navbar-collaspe" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">{!! link_to_route('tasks.create','新規タスクの入力',[],['class'=>'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_tasks')
+            
             @yield('content')
         </div>
         
